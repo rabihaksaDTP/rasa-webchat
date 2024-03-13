474fbce3b444a64c395d71bacdd8e05e9f4fe0c6
mfenej <56250083+mfenej@users.noreply.github.com>
2024-03-13 15:51:19 +0200

    Revert "try polling transport for socket.io"
    This reverts commit de2d1b3f57871d8886947b256b1f5ec6945fa3f5.

--- src/socket-socketio.js
+++ src/socket-socketio.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 export default function (socketUrl, customData, path) {
   // const options = path ? { path } : {};
-  const socket = io(socketUrl, { transports: ['polling'] });
+  const socket = io(socketUrl, { transports: ['websocket'] });
   socket.on('connect', () => {
     console.log(`connect:${socket.id}`);
     socket.customData = customData;
